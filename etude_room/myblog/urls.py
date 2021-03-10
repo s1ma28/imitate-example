a4a32c91d6498af097d1bc04bc338a6ba8f61850
s1ma28 <s1ma28g@gmail.com>
2021-03-10 20:04:23 +0900

    Material Design BootStrapのセッティング終了

--- etude_room/myblog/urls.py
+++ etude_room/myblog/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'myblog'
 
 urlpatterns = [
-    path('', views.Index, name='index'),
+    path('', views.Index.as_view(), name='index'),
 ]
--- etude_room/myblog/views.py
+++ etude_room/myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def Index(request):
-    return HttpResponse('HelloWorld')
+class Index(TemplateView):
+    template_name = 'myblog/index.html'
